a7b953b4caef2c5d57a98c8c3130c38152288e70
gabzerbinato <115713279+gabzerbinato@users.noreply.github.com>
2023-11-16 22:57:21 -0300

    Create Veiculo.java

---
+++ Veiculo.java
@@ -0,0 +1,13 @@
+public class Veiculo {
+	
+	String modelo;
+	String placa;
+	String cor;
+
+	public Veiculo(String modelo, String placa, String cor) {
+		this.modelo = modelo;
+		this.placa = placa;
+		this.cor = cor;
+	}
+
+}
